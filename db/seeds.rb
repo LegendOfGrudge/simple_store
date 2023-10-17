493eff07c528c4c6ade47cad95da374a9c82d26a
Nick <nicholasfletcher3682@yahoo.ca>
2023-10-17 12:38:53 -0500

    Wrote products from a .csv file and associated them with categories

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,11 +5,35 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+require "csv"
 
 Product.destroy_all
+Category.destroy_all
 
-676.times {
-  Product.create(title: Faker::Commerce.product_name, price: Faker::Commerce.price, stock_quantity: Faker::Number.number(digits: 3))
-}
+csv_file = Rails.root.join('db/products.csv')
+csv_data = File.read(csv_file)
+
+products = CSV.parse(csv_data, headers: true)
+
+# If CSV was created by Excel in Windows you may also need to set an encoding type:
+# products = CSV.parse(csv_data, headers: true, encoding: 'iso-8859-1')
+
+products.each do |product|
+  category = Category.find_or_create_by(name: product['category'])
+
+  if category && category.valid?
+    newProduct = category.products.create(
+      title: product['name'],
+      price: product['price'],
+      description: product['description'],
+      stock_quantity: product['stock quantity'],
+      category:
+    )
+  end
+end
+
+# 676.times {
+#   Product.create(title: Faker::Commerce.product_name, price: Faker::Commerce.price, stock_quantity: Faker::Number.number(digits: 3))
+# }
 
 puts "Created #{Product.count} Products"
